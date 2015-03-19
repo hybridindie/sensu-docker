f41970d2d65816c816eb693bdb33c40c01986ffc
John Dilts <iam@hybridindie.com>
2015-03-19 15:28:24 -0400

    updaing openssl and optomise the build process

--- Grafana/install-sensu.sh
+++ Grafana/install-sensu.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+curl https://www.openssl.org/source/openssl-1.0.1h.tar.gz | tar xz && \
+            cd openssl-1.0.1h && sudo ./config && sudo make && sudo make install
 wget -q http://repos.sensuapp.org/apt/pubkey.gpg -O- | apt-key add -
 echo "deb http://repos.sensuapp.org/apt sensu main" > /etc/apt/sources.list.d/sensu.list
 
-apt-get update && apt-get install -y git-core sensu
+apt-get update && apt-get install -y sensu
 echo "sensu hold" | dpkg --set-selections
 
 echo "EMBEDDED_RUBY=true" > /etc/default/sensu & ln -s /opt/sensu/embedded/bin/ruby /usr/bin/ruby
+echo "LOG_LEVEL=info" > /etc/default
 /opt/sensu/embedded/bin/gem install redphone --no-rdoc --no-ri
 /opt/sensu/embedded/bin/gem install mail --no-rdoc --no-ri --version 2.5.4
 
@@ -14,6 +17,3 @@ git clone https://github.com/sensu/sensu-community-plugins.git /tmp/sensu_plugin
 
 cp -Rpf /tmp/sensu_plugins/plugins /etc/sensu/
 find /etc/sensu/plugins/ -name *.rb -exec chmod +x {} \;
-
-mkdir -p /etc/sensu/ssl
-cp /tmp/ssl_certs/client/cert.pem /tmp/ssl_certs/client/key.pem /etc/sensu/ssl
