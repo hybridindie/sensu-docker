ee6101ccd9f2f224bd4938634e261f33a57b36c8
John Dilts <john@76golf.com>
2015-03-19 20:27:21 -0400

    code cleanup

--- Grafana/install-sensu.sh
+++ Grafana/install-sensu.sh
@@ -15,11 +15,3 @@ git clone https://github.com/sensu/sensu-community-plugins.git /tmp/sensu_plugin
 
 cp -Rpf /tmp/sensu_plugins/plugins /etc/sensu/
 find /etc/sensu/plugins/ -name *.rb -exec chmod +x {} \;
-
-# curl https://www.openssl.org/source/openssl-1.0.1h.tar.gz | tar xz && \
-#             cd openssl-1.0.1h && ./config && make && make install
-# ln -sf /usr/local/ssl/bin/openssl `which openssl`
-# cd ../ && rm -rf openssl-1.0.1h
-#
-# apt-get remove -y --auto-remove build-essential
-# apt-get purge -y --auto-remove build-essential
