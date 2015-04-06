b4b2ee46dfe715cbf6fbedd89ba55c0f1930e1a7
John Dilts <john@76golf.com>
2015-04-06 16:00:52 -0400

    started to move files and data to datastore

--- support/install-sensu.sh
+++ support/install-sensu.sh
@@ -1,32 +1,4 @@
 #!/bin/bash
-if [ -f "/usr/local/etc/sensu-docker/sensu_ca/serial" ]; then
-  echo "Certificates already generated"
-else
-  mkdir /usr/local/etc/sensu-docker && cd /usr/local/etc/sensu-docker
-  mkdir -p client server sensu_ca/private sensu_ca/certs
-  passwd=$(openssl rand -base64 32 | base64 | head -c 24 ; echo)
-  touch sensu_ca/index.txt
-  echo 01 > sensu_ca/serial
-  cd sensu_ca
-  openssl req -x509 -config /usr/local/etc/sensu-docker/openssl.cnf -newkey rsa:2048 -days 1825 -out cacert.pem -outform PEM -subj /CN=SensuCA/ -nodes
-  openssl x509 -in cacert.pem -out cacert.cer -outform DER
-  cd ../server
-  openssl genrsa -out key.pem 2048
-  openssl req -new -key key.pem -out req.pem -outform PEM -subj /CN=sensu/O=server/ -nodes
-  cd ../sensu_ca
-  openssl ca -config /usr/local/etc/sensu-docker/openssl.cnf -in ../server/req.pem -out ../server/cert.pem -batch -extensions server_ca_extensions
-  cd ../server
-  openssl pkcs12 -export -out keycert.p12 -in cert.pem -inkey key.pem -passout pass:$passwd
-  cd ../client
-  openssl genrsa -out key.pem 2048
-  openssl req -new -key key.pem -out req.pem -outform PEM -subj /CN=sensu/O=client/ -nodes
-  cd ../sensu_ca
-  openssl ca -config /usr/local/etc/sensu-docker/openssl.cnf -in ../client/req.pem -out ../client/cert.pem -batch -extensions client_ca_extensions
-  cd ../client
-  openssl pkcs12 -export -out keycert.p12 -in cert.pem -inkey key.pem -passout pass:$passwd
-  cd ../../
-fi
-
 wget -q http://repos.sensuapp.org/apt/pubkey.gpg -O- | apt-key add -
 echo "deb http://repos.sensuapp.org/apt sensu main" > /etc/apt/sources.list.d/sensu.list
 
