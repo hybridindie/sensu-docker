1375631bda804887c145a99d5506f4241ec476c7
John Dilts <john@76golf.com>
2015-04-08 08:27:23 -0400

    update to install client script

--- install_client.sh
+++ install_client.sh
@@ -31,7 +31,7 @@ cat << EOF > /etc/sensu/config.json
     "port": 5671,
     "host": "%RABBITMQ_ADDR_OR_IP%",
     "user": "sensu",
-    "password": "pass",
+    "password": "%RABBITMQ_PASSWD%",
     "vhost": "/sensu"
   },
   "client": {
@@ -42,4 +42,12 @@ cat << EOF > /etc/sensu/config.json
 }
 EOF
 
-echo "Now go and modify /etc/sensu/config.json with your Sensu RabbitMQ host and this nodes specifics."
+echo "Next steps are:"
+echo "* Retrieve the RabbitMQ password from /usr/local/etc/sensu-docker/sensu.env on the server."
+echo "* modify /etc/sensu/config.json with your Sensu RabbitMQ host and password."
+echo "* Create a unique name and add the hostname of this node for the 'client' section of /etc/sensu/config.json."
+echo "* mkdir -p /etc/sensu/ssl"
+echo "* copy /usr/local/etc/client/{cert,key}.pem from the server to /etc/sensu/ssl"
+echo "* sudo service sensu-client start"
+echo ""
+echo "Now watch the Uchiwa dashbard of the server for the node to join."
