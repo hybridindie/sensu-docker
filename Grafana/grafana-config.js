2b7eaf30eeb782a9973b8d4c7976be9dd2f1051f
John Dilts <john@76golf.com>
2015-04-14 17:36:20 -0400

    added files for rollback of grafana version

---
+++ Grafana/grafana-config.js
@@ -0,0 +1,64 @@
+// == Configuration
+// config.js is where you will find the core Grafana configuration. This file contains parameter that
+// must be set before Grafana is run for the first time.
+
+define(['settings'], function(Settings) {
+
+
+  return new Settings({
+
+  datasources: {
+    'sensu': {
+      type: 'influxdb',
+      url: "http://%INFLUXDB_HOST%:%INFLUXDB_PORT%/db/sensu",
+      username: 'sensu',
+      password: '%INFLUXDB_SENSU_PASSWD%'
+    },
+    'grafana': {
+      type: 'influxdb',
+      url: "http://%INFLUXDB_HOST%:%INFLUXDB_PORT%/db/grafana",
+      username: 'grafana',
+      password: '%INFLUXDB_GRAFANA_PASSWD%',
+      grafanaDB: true
+    }
+  },
+
+  /* Global configuration options
+  * ========================================================
+  */
+
+  // specify the limit for dashboard search results
+  search: {
+    max_results: 100
+  },
+
+  // default home dashboard
+  default_route: '/dashboard/file/default.json',
+
+  // set to false to disable unsaved changes warning
+  unsaved_changes_warning: true,
+
+  // set the default timespan for the playlist feature
+  // Example: "1m", "1h"
+  playlist_timespan: "1m",
+
+  // If you want to specify password before saving, please specify it below
+  // The purpose of this password is not security, but to stop some users from accidentally changing dashboards
+  admin: {
+    password: ''
+  },
+
+  // Change window title prefix from 'Grafana - <dashboard title>'
+  window_title_prefix: 'SENSU DOCKER - ',
+
+  // Add your own custom panels
+  plugins: {
+    // list of plugin panels
+    panels: [],
+    // requirejs modules in plugins folder that should be loaded
+    // for example custom datasources
+    dependencies: [],
+  }
+
+  });
+});
