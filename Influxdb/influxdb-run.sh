904931ef5bdc3e487859dbcc97c09dd55ad6ac7b
John Dilts <john@76golf.com>
2015-04-24 15:08:12 -0400

    update to Influxdb scripts and turned off shared data volume until i figure out why it can write there

--- Influxdb/influxdb-run.sh
+++ Influxdb/influxdb-run.sh
@@ -19,33 +19,36 @@ cat << EOF > /etc/sensu/influxdb-config.json
   }
 }
 EOF
+if [ -f "/usr/local/etc/sensu-docker/.influxdb_configured" ]; then
+  echo "InfluxDB already to go"
+else
+  /etc/init.d/influxdb start
 
-/etc/init.d/influxdb start
+  until (curl -X POST 'http://localhost:8086/cluster_admins/root?u=root&p=root' \
+              -d '{"password": "'"$INFLUXDB_ROOT_PASSWD"'"}' 2>/dev/null) \
+              do sleep 1; done
+  echo 'Changed "root" password'
 
-until (curl -X POST 'http://localhost:8086/cluster_admins/root?u=root&p=root' \
-            -d '{"password": "'"$INFLUXDB_ROOT_PASSWD"'"}' 2>/dev/null) \
-            do sleep 1; done
-echo 'Changed "root" password'
+  until (curl -X POST 'http://localhost:8086/db?u=root&p='"$INFLUXDB_ROOT_PASSWD" \
+              -d '{"name": "sensu"}' 2>/dev/null) do sleep 1; done
+  echo 'Created database "sensu"'
 
-until (curl -X POST 'http://localhost:8086/db?u=root&p='"$INFLUXDB_ROOT_PASSWD" \
-            -d '{"name": "sensu"}' 2>/dev/null) do sleep 1; done
-echo 'Created database "sensu"'
+  until (curl -X POST 'http://localhost:8086/db/sensu/users?u=root&p='"$INFLUXDB_ROOT_PASSWD" \
+              -d '{"name": "sensu", "password": "'"$INFLUXDB_SENSU_PASSWD"'"}' 2>/dev/null) \
+              do sleep 1; done
+  echo 'Created User "sensu"'
 
-until (curl -X POST 'http://localhost:8086/db/sensu/users?u=root&p='"$INFLUXDB_ROOT_PASSWD" \
-            -d '{"name": "sensu", "password": "'"$INFLUXDB_SENSU_PASSWD"'"}' 2>/dev/null) \
-            do sleep 1; done
-echo 'Created User "sensu"'
+  until (curl -X POST 'http://localhost:8086/db?u=root&p='"$INFLUXDB_ROOT_PASSWD" \
+              -d '{"name": "grafana"}' 2>/dev/null) do sleep 1; done
+  echo 'Created database "grafana"'
 
-until (curl -X POST 'http://localhost:8086/db?u=root&p='"$INFLUXDB_ROOT_PASSWD" \
-            -d '{"name": "grafana"}' 2>/dev/null) do sleep 1; done
-echo 'Created database "grafana"'
+  until (curl -X POST 'http://localhost:8086/db/grafana/users?u=root&p='"$INFLUXDB_ROOT_PASSWD" \
+              -d '{"name": "grafana", "password": "'"$INFLUXDB_GRAFANA_PASSWD"'"}' 2>/dev/null) \
+              do sleep 1; done
+  echo 'Created User "grafana"'
 
-until (curl -X POST 'http://localhost:8086/db/grafana/users?u=root&p='"$INFLUXDB_ROOT_PASSWD" \
-            -d '{"name": "grafana", "password": "'"$INFLUXDB_GRAFANA_PASSWD"'"}' 2>/dev/null) \
-            do sleep 1; done
-echo 'Created User "grafana"'
-
-/etc/init.d/influxdb stop
-sleep 3
-
+  touch "/usr/local/etc/sensu-docker/.influxdb_configured"
+  /etc/init.d/influxdb stop
+  sleep 3
+fi
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
